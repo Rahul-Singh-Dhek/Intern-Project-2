01bfd17ce326e2f4b43dbae83ce8f4456b3df1d4
Pratibha <pratibhad65@gmail.com>
2022-09-13 17:18:18 +0530

    internSchema

---
+++ src/models/internModel.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const internSchema = new mongoose.Schema({
+name:{
+    type:String,
+    require:true,
+    trim:true
+},
+email:{
+    type:String,
+    trim:true,
+    lowercase:true,
+    unique:true
+},
+    mobile:{
+        type:Number,
+        require:true,
+        unique:true,
+        trim:true
+    },
+    collegeId:{
+        type:mongoose.Types.ObjectId, ref:"college",
+        require:true
+
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false
+    },
+    isDeleted:{
+     type:Date,
+     default:null
+    }
+
+
+},{timestamps:true})
+
+
+
+
+
+module.exports=mongoose.model("Intern",internSchema)
--- src/routes/route.js
+++ src/routes/route.js
@@ -11,4 +11,11 @@ const router = express.Router();
 
 
 
+
+
+
+
+
+
+
 module.exports = router;
