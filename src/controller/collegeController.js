0dcc56f7ce21ea86771f875bbcaf9bb4cb32f0b1
Rahul-Singh-Dhek <rahuldhek21@gmail.com>
2022-09-17 08:48:14 +0530

    RD

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -6,27 +6,34 @@ const validator = require("../validators/validators")
 
 
 const createCollege = async function (req, res) {
+
+
     try {
         let body = req.body
+
+        let test = ""
         if ((Object.keys(req.body)).length == 0) {
             return res.status(400).send({ status: false, message: "Please provide details of the college" })
         }
+        //------------------------------------------------------------------------------------------------------------------------------
         if (!body.name) {
-            return res.status(400).send({ status: false, message: "Please provide name of the college" })
+            test = "Please provide name of the college"
+        } else {
+            body.name = body.name.toLowerCase()
+            if (!validator.isValidShortName(body.name)) {
+                test = (test.length == 0) ? "name can contain only letters" : test + " ; " + "name can contain only letters"
+            } else {
+                let name = await collegeModel.findOne({ name: body.name, isDeleted: false })
+                if (name) {
+                    test = (test.length == 0) ? "Please provide unique name of the college" : test + " ; " + "Please provide unique name of the college"
+                }
+            }
         }
-        body.name=body.name.toLowerCase()
-        if (!validator.isValidShortName(body.name)) {
-            return res.status(400).send({ status: false, message: "Name can contain only letters" })
-        }
-        let name = await collegeModel.findOne({ name: body.name,isDeleted:false })
-        if (name) {
-            return res.status(400).send({ status: false, message: "Please provide unique name" })
-        }
+        //-----------------------------------------------------------------------------------------------------------------------
         if (!body.fullName) {
-            return res.status(400).send({ status: false, message: "Please provide fullName of the college" })
-        }
-        if (!validator.isValidFullName(body.fullName)) {
-            return res.status(400).send({ status: false, message: "fullName can contain only letters,space,comma,'&',and '-' " })
+            test = (test.length == 0) ? "Please provide fullName of the college" : test + " ; " + "Please provide fullName of the college"
+        }else if (!validator.isValidFullName(body.fullName)) {
+            test = (test.length == 0) ? "fullName can contain only letters,space,comma,'&',and '-' " : test + " ; " + "fullName can contain only letters,space,comma,'&',and '-' "
         }
         //-----------------------------------------------------------------------------------------------------------------
         // let fullName = await collegeModel.findOne({ fullName: body.fullName,isDeleted:false })
@@ -35,11 +42,12 @@ const createCollege = async function (req, res) {
         // }
         //-----------------------------------------------------------------------------------------------------------------
         if (!body.logoLink) {
-            return res.status(400).send({ status: false, message: "Please provide logoLink of the college" })
+            test = (test.length == 0) ? "Please provide logoLink of the college" : test + " ; " + "Please provide logoLink of the college"
+        } else if (!validator.isValidLink(body.logoLink)) {
+            test = (test.length == 0) ? "logoLink is invalid" : test + " ; " + "logoLink is invalid"
         }
-        if(!validator.isValidLink(body.logoLink)){
-            return res.status(400).send({status:false,message:"LogoLink is invalid"})
-        }
+        //----------------------------------------------------------------------------------------------------------------------
+        if (test) return res.status(400).send({ status: false, message: test })
         let data = await collegeModel.create(body)
         return res.status(201).send({ status: true, data: data })
     }
@@ -48,6 +56,7 @@ const createCollege = async function (req, res) {
     }
 }
 
+//--------------------------------------------------------------------------------------------------------------------------------
 const collegeDetails = async function (req, res) {
     try {
         let collegeName = req.query.collegeName
@@ -57,24 +66,19 @@ const collegeDetails = async function (req, res) {
         if (!validator.isValidShortName(collegeName)) {
             return res.status(400).send({ status: false, message: "collegeName can contain only letters" })
         }
-        let collegeDetail = await collegeModel.findOne({ name: collegeName.toLowerCase() , isDeleted:false})
+        let collegeDetail = await collegeModel.findOne({ name: collegeName.toLowerCase(), isDeleted: false })
         if (!collegeDetail) {
             return res.status(404).send({ status: false, message: "No college exists with this Name" })
         }
         let ColId = collegeDetail["_id"]
-        let Interns = await internModel.find({ collegeId: ColId ,isDeleted:false}).select({name:1,email:1,mobile:1})
-        //-----------------------------------------------------------------------------------------------------------
-        // if (Interns.length == 0) {
-        //     return res.status(404).send({ status: false, message: "No interns found of this college" })
-        // }
-        //------------------------------------------------------------------------------------------------------------
-        let data={name:collegeDetail.name,fullName:collegeDetail.fullName,logoLink:collegeDetail.logoLink,interns:Interns}
-        return res.status(200).send({data:data,status:true})
+        let Interns = await internModel.find({ collegeId: ColId, isDeleted: false }).select({ name: 1, email: 1, mobile: 1 })
+        let data = { name: collegeDetail.name, fullName: collegeDetail.fullName, logoLink: collegeDetail.logoLink, interns: Interns }
+        return res.status(200).send({ data: data, status: true })
     }
     catch (err) {
         return res.status(500).send({ status: false, message: err.message })
     }
 }
 
-module.exports.collegeDetails=collegeDetails
+module.exports.collegeDetails = collegeDetails
 module.exports.createCollege = createCollege
--- src/controller/internController.js
+++ src/controller/internController.js
@@ -10,25 +10,59 @@ const createIntern = async function (req, res) {
         if (Object.keys(data).length == 0) return res.status(404).send({ status: false, msg: "Please provide Intern Details." })
         let { name, email, mobile, collegeName } = data
         //edge cases
-        if (!name) return res.status(400).send({ status: false, msg: "Intern name must be required." })
-        if (!validator.isValidName(name)) return res.status(400).send({ status: false, msg: "Intern name must consist of only leters." })
-        
-        if (!mobile) return res.status(400).send({ status: false, msg: "mobile must be required." })
-        if (!validator.isValidMobile(mobile)) return res.status(400).send({ status: false, msg: "Please enter valid mobile." })
-        let doc= await internModel.findOne({ mobile: mobile,isDeleted:false })
-        if (doc) return res.status(400).send({ status: false, msg: "mobile is already registered." })
-        
-        if (!email) return res.status(400).send({ status: false, msg: "email must be required." })
-        if (!validator.isValidEmail(email)) return res.status(400).send({ status: false, msg: "Please enter valid email." })
-        doc = await internModel.findOne({ email: email,isDeleted:false })
-        if (doc) return res.status(400).send({ status: false, msg: "email is already registered." })
+        let test = ""
+        //------------------------------------------------------------------------------------------------------------------------------        
+        if (!name) {
+            test = "Intern name must be required"
+        } else if (!validator.isValidName(name)) {
+            test = (test.length == 0) ? "Intern name must consist of only leters" : test + " ; " + "Intern name must consist of only leters"
+        }
+        //------------------------------------------------------------------------------------------------------------------------------        
+        if (!email) {
+            test = (test.length == 0) ? "email must be required" : test + " ; " + "email must be required"
+        } else {
+            if (!validator.isValidEmail(email)) {
+                test = (test.length == 0) ? "Please enter valid email" : test + " ; " + "Please enter valid email"
+            } else {
+                let doc = await internModel.findOne({ email: email, isDeleted: false })
+                if (doc) {
+                    test = (test.length == 0) ? "email is already registered" : test + " ; " + "email is already registered"
+                }
+            }
+        }
+        //------------------------------------------------------------------------------------------------------------------------------        
+        if (!mobile) {
+            test = (test.length == 0) ? "mobile must be required" : test + " ; " + "mobile must be required"
+        } else {
+            if (!validator.isValidMobile(mobile)) {
+                test = (test.length == 0) ? "Please enter valid mobile" : test + " ; " + "Please enter valid mobile"
+            } else {
+                doc = await internModel.findOne({ mobile: mobile, isDeleted: false })
+                if (doc) {
+                    test = (test.length == 0) ? "mobile is already registered" : test + " ; " + "mobile is already registered"
+                }
+            }
+        }
+        //------------------------------------------------------------------------------------------------------------------------------        
+        let collegeDetails;
+        if (!collegeName) {
+            test = (test.length == 0) ? "collegeName must be required" : test + " ; " + "collegeName must be required"
+        } else {
+            if (!validator.isValidShortName(collegeName)) {
+                test = (test.length == 0) ? "collegeName can contain only letters" : test + " ; " + "collegeName can contain only letters"
+            } else {
+                collegeDetails = await collegeModel.findOne({ name: collegeName.toLowerCase(), isDeleted: false })
+                if (!collegeDetails) {
+                    test = (test.length == 0) ? "college not found..Please try with another college Name" : test + " ; " + "college not found..Please try with another college Name"
+                }
+            }
+        }
+        //------------------------------------------------------------------------------------------------------------------------------        
+        if (test) {
+            return res.status(400).send({ status: false, msg: test })
+        }
+        data["collegeId"] = collegeDetails["_id"]
 
-        if (!collegeName) return res.status(400).send({ status: false, msg: "collegeName must be required." })
-        if (!validator.isValidShortName(collegeName)) return res.status(400).send({ status: false, message: "collegeName can contain only letters" })
-        let collegeDetails = await collegeModel.findOne({ name: collegeName.toLowerCase() ,isDeleted:false})
-        if (!collegeDetails) return res.status(400).send({ status: false, msg: "college not found..Please try with another college Name." })
-        data["collegeId"]  = collegeDetails["_id"]
-        
         let result = await internModel.create(data)
         return res.status(201).send({ status: true, data: result })
     }
